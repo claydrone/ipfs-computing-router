a5a8a14c793417eec87ecb8a401d04c2ee835861
Charles Cao <nebulablock@gmail.com>
2022-09-11 21:34:40 -0400

    Choose MCS/ipfs.io for submitting result.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # ipfs-computing-router
+
 IPFS Computing Router enable user to store and fetch dataset from the closest IPFS node and upload the training result
 to the same node available.
-	
+
+## Installation
+
+```shell
+$ pip install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -1,24 +1,52 @@
+import os, time
+from mcs.contract import ContractAPI
+from mcs.api import McsAPI
 import requests
 import socket
+from dotenv import load_dotenv
 
-mcs_url = 'mcs.filswan.com'
-mcs_ip = socket.gethostbyname(mcs_url)
-mcs_response_time = requests.get('https://' + mcs_url).elapsed.total_seconds()
-mcs_url_location = requests.get('https://ipinfo.io/' + mcs_ip)
-# print(mcs_url, mcs_url_location.json())
-print(mcs_url, mcs_url_location.json()['city'])
+load_dotenv()
+web3_api=os.getenv('web3_api')
+wallet_address=os.getenv('wallet_address')
+private_key=os.getenv('private_key')
 
-ipfs_io_url = 'ipfs.io'
-ipfs_io_ip = socket.gethostbyname(ipfs_io_url)
-ipfs_io_response_time = requests.get('https://' + mcs_url).elapsed.total_seconds()
+def upload_file_pay(filepath):
+    w3_api = ContractAPI(web3_api)
+    api = McsAPI()
+    # upload file to mcs
+    father_path = os.path.abspath(os.path.dirname(__file__))
+    upload_file = api.upload_file(wallet_address, father_path + filepath)
+    file_data = upload_file["data"]
+    payload_cid, source_file_upload_id, nft_uri, file_size, w_cid = file_data['payload_cid'], file_data[
+        'source_file_upload_id'], file_data['ipfs_url'], file_data['file_size'], file_data['w_cid']
+    # get the global variable
+    params = api.get_params()["data"]
+    # get filcoin price
+    rate = api.get_price_rate()["data"]
+    # test upload_file_pay contract
+    w3_api.upload_file_pay(wallet_address, private_key, file_size, w_cid, rate, params)
 
-ipfs_io_location = requests.get('https://ipinfo.io/' + ipfs_io_ip)
-print(ipfs_io_url, ipfs_io_location.json()['city'])
 
-print('ipfs_io_response_time', ipfs_io_response_time)
-print('mcs_response_time', mcs_response_time)
+if __name__ == "__main__":
+    mcs_url = 'mcs.filswan.com'
+    mcs_ip = socket.gethostbyname(mcs_url)
+    mcs_response_time = requests.get('https://' + mcs_url).elapsed.total_seconds()
+    mcs_url_location = requests.get('https://ipinfo.io/' + mcs_ip)
+    # print(mcs_url, mcs_url_location.json())
+    print(mcs_url, mcs_url_location.json()['city'])
 
-if mcs_response_time < ipfs_io_response_time:
-    print('Chose mcs gateway')
-else:
-    print('Chose ipfs gateway')
+    ipfs_io_url = 'ipfs.io'
+    ipfs_io_ip = socket.gethostbyname(ipfs_io_url)
+    ipfs_io_response_time = requests.get('https://' + mcs_url).elapsed.total_seconds()
+
+    ipfs_io_location = requests.get('https://ipinfo.io/' + ipfs_io_ip)
+    print(ipfs_io_url, ipfs_io_location.json()['city'])
+
+    print('ipfs_io_response_time', ipfs_io_response_time)
+    print('mcs_response_time', mcs_response_time)
+
+    if mcs_response_time < ipfs_io_response_time:
+        print('Chose mcs gateway')
+        upload_file_pay('/computing/image_classification.ipynb')
+    else:
+        print('Chose ipfs gateway')
