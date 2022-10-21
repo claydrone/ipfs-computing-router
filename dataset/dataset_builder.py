0bbf1dccab510159ffe8b7203ac8fca887c9ac8c
Charles Cao <nebulablock@gmail.com>
2022-10-20 22:11:02 -0400

    update file type

--- dataset/dataset_builder.py
+++ dataset/dataset_builder.py
@@ -1,5 +1,7 @@
 from dataset import Dataset
 from file import File
 
-files=[File('monkey','../input/bestsellers_with_categories.csv')]
+files=File('monkey','../input/bestsellers_with_categories.csv')
+files.upload_file_pay()
+
 print(Dataset("bestsellers_with_categories",files).title)
--- dataset/file.py
+++ dataset/file.py
@@ -15,14 +15,11 @@ class File:
         private_key = os.getenv('private_key')
         rpc_endpoint = os.getenv('rpc_endpoint')
 
-        up = MCSUpload("polygon.mainnet", wallet_address, private_key, rpc_endpoint, self.path)
-        up.approve_token(1)
-        file_data, need_pay = up.stream_upload()
-        print(up.estimate_amount)
+        uploaded_file = MCSUpload("polygon.mainnet", wallet_address, private_key, rpc_endpoint, self.path)
+        uploaded_file.approve_token(1)
+        file_data, need_pay = uploaded_file.stream_upload()
+        print(uploaded_file.upload_response)
 
         if need_pay:
-            up.pay()
-            up.mint('a_image')
-
-
-
+            uploaded_file.pay()
+            uploaded_file.mint('a_image')
